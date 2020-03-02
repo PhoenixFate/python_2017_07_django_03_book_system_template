5a3df68132056dc26d28e6a29bcef271fbbd6610
phoenixfate <sm516116978@outlook.com>
2020-03-03 01:39:02 +0800

    登陆操作

--- bookInfo/urls.py
+++ bookInfo/urls.py
@@ -18,10 +18,15 @@ from . import views
 
 urlpatterns = [
     path("", views.index1),
+    path("index/", views.index1),
     path("index2/", views.index2),
     path("temp/", views.temp_var),
     path("temp_tags/", views.template_tag),
     path("temp_filter/", views.template_filter),
     path("temp_child1/", views.template_extend),
-    path("html_escape/",views.html_escape)
+    path("html_escape/", views.html_escape),
+    path("login/", views.login),
+    path("login_check/", views.login_check),
+    path("change_password/", views.change_password),
+    path("change_password_action/", views.change_password_action),
 ]
--- bookInfo/views.py
+++ bookInfo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader, RequestContext
 from django.http import HttpResponse
 from .models import Book
@@ -64,3 +64,69 @@ def html_escape(request):
     """html 转义"""
 
     return render(request, "bookInfo/escape.html", {'content': "<h1>hello</h1>"})
+
+
+def login(request):
+    """显示登录页面"""
+    # 判断用户是否已经登录
+    if request.session.get("user"):
+        return redirect("/index")
+    # 取出cookie中的值
+    if "username_cookie" in request.COOKIES:
+        username_cookie = request.COOKIES["username_cookie"]
+    else:
+        username_cookie = ""
+    return render(request, "bookInfo/login.html", {"username_cookie": username_cookie})
+
+
+def login_check(request):
+    """校验登录"""
+    # request.POST 保存的是post提交的参数
+    # request.GET 保存的是get提交的参数
+    request_path = request.path
+    print(request_path)
+    method_type = request.method
+    print(method_type)
+    # 1.获取用户名和密码
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+    remember = request.POST.get("remember")
+    gender = request.POST.get('gender')
+    is_tuanyuan = request.POST.get('is_tuanyuan')
+    joy = request.POST.getlist('joy')
+    city = request.POST.get('city')
+    more_text = request.POST.get('more_text')
+    print("username: %s " % username)
+    print("password: %s " % password)
+    print("remember: ", remember)
+    print("gender: ", gender)
+    print("is_tuanyuan: ", is_tuanyuan)
+    print("joy: ", joy)
+    print("city: ", city)
+    print("more_text: ", more_text)
+    # 2.进行登录的校验
+    if username == "admin" and password == "123456":
+        response = redirect("/index")
+        # 判断是否记住用户名
+        if remember == "on":
+            response.set_cookie("username_cookie", username, max_age=7 * 24 * 3600)
+        # 记住登录状态
+        request.session["user"] = {"username": username, "password": password}
+        return response
+    # 3.返回应答
+    return redirect("/login/")
+
+
+def change_password(request):
+    """显示修改密码页面"""
+    if not request.session.get("user"):
+        return redirect("/login")
+    return render(request, "bookInfo/change_password.html", {})
+
+
+def change_password_action(request):
+    """修改密码action"""
+    new_password = request.POST.get("new_password")
+    print("new_password:%s" % new_password)
+    user = request.session.get("user")
+    return HttpResponse("%s 修改密码为： %s" % (user["username"], new_password))
